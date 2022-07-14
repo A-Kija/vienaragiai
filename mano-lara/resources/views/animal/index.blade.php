3d95a0ca930b76f57c018d004707cb4a68d68fc9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-14 10:05:56 +0300

    role

--- mano-lara/resources/views/animal/index.blade.php
+++ mano-lara/resources/views/animal/index.blade.php
@@ -1,43 +1,37 @@
-@extends('main')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-               <div class="card-header">PAVADINIMAS</div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">My fantastic Zoo</div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @forelse($animals as $animal)
+                        <li class="list-group-item">
+                            <div class="color-box2" style="background:{{$animal->getThisAnimalsColor_plese->color}};">
+                                <i>{{$animal->getThisAnimalsColor_plese->title}}</i>
+                                <h2>{{$animal->name}}</h2>
+                            </div>
+                            <div class="controls">
+                                <a class="btn btn-outline-primary m-2" href="{{route('animals-show', $animal->id)}}">Show</a>
+                                <a class="btn btn-outline-success m-2" href="{{route('animals-edit', $animal)}}">Edit</a>
+                                <form class="delete" action="{{route('animals-delete', $animal)}}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-outline-danger m-2">Destroy</button>
+                                </form>
+                            </div>
 
-               <div class="card-body">
-                 BLADE TURINYS
-               </div>
-           </div>
-       </div>
-   </div>
+                        </li>
+                        @empty
+                        <li class="list-group-item">No animals, no life.</li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-@endsection
-
-
-@section('content')
-<ul>
-    @forelse($animals as $animal)
-    <li>
-        <div class="color-box" style="background:{{$animal->getThisAnimalsColor_plese->color}};">
-        {{$animal->getThisAnimalsColor_plese->title}}
-        <h2>{{$animal->name}}</h2>
-    </div>
-        <div class="controls">
-        <a href="{{route('animals-show', $animal->id)}}">SHOW</a>
-        <a href="{{route('animals-edit', $animal)}}">EDIT</a>
-        <form class="delete" action="{{route('animals-delete', $animal)}}" method="post">
-            @csrf
-            @method('delete')
-            <button type="submit">Kill</button>
-        </form>
-    </div>
-    </li>
-    @empty
-    <li>No animals, no life.</li>
-    @endforelse
-</ul>
-
 @endsection
